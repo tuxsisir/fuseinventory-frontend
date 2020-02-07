35fdd2bd36cc3732c7ace656dea7c3a920ac99ca
Sisir Ghimire Chettri <samratsisir294@gmail.com>
2020-02-07 23:05:44 +0545

    Integrate Inventory

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -23,11 +23,31 @@
               <v-tab>Non Consumable</v-tab>
             </v-tabs>
             <v-data-table
+              :headers="headers"
               :items="inventoryList"
               class="elevation-1"
               hide-default-footer
-              hide-default-header
-              ></v-data-table>
+              >
+              <template slot="item"
+                        slot-scope="props">
+                <tr>
+                  <td>
+                    {{ props.item.name }}
+                    <div>{{ props.item.inventoryType }}</div>
+                  </td>
+                  <td>{{ props.item.description }}</td>
+                  <td>{{ props.item.quantity }}</td>
+                  <td>
+                    <v-btn small fab icon class="mr-1" @click="triggerUpdate(props.item)">
+                      <v-icon small>mdi-pencil</v-icon>
+                    </v-btn>
+                    <v-btn small fab icon @click="deleteInventory(props.item)">
+                      <v-icon small>mdi-delete</v-icon>
+                    </v-btn>
+                  </td>
+                </tr>
+              </template>
+            </v-data-table>
           </v-card-text>
         </v-card>
       </v-col>
@@ -49,19 +69,24 @@
               <v-container>
                 <v-row>
                   <v-col cols="12">
-                    <v-text-field v-model="formValues.username" label="User Name *" required></v-text-field>
+                    <v-text-field v-model="formValues.name" label="Item Name *" required></v-text-field>
                   </v-col>
                   <v-col cols="12">
-                    <v-text-field v-model="formValues.fullName" label="Full Name *" required></v-text-field>
+                    <v-text-field v-model="formValues.description" label="Item Description *" required></v-text-field>
                   </v-col>
                   <v-col cols="6">
-                    <v-text-field v-model="formValues.email" label="Email *" required></v-text-field>
+                    <v-text-field
+                      v-model="formValues.quantity"
+                      type="number"
+                      min="1"
+                      label="Quantity *"
+                      required></v-text-field>
                   </v-col>
                   <v-col cols="6">
                     <v-select
-                      v-model="formValues.gender"
-                      :items="['Male', 'Female']"
-                      label="Gender *"
+                      v-model="formValues.inventoryType"
+                      :items="[`Consumable`, `Non-Consumable`]"
+                      label="Type *"
                       required
                       ></v-select>
                   </v-col>
@@ -71,7 +96,7 @@
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="userFormDialog = false">Close</v-btn>
+              <v-btn color="blue darken-1" text @click="inventoryFormDialog = false">Close</v-btn>
               <v-btn color="blue darken-1" text @click="getFormAction()">Save</v-btn>
             </v-card-actions>
           </v-card>
@@ -86,6 +111,8 @@ import axios from 'axios';
 export default {
   data() {
     return {
+      snackbar: false,
+      snackbarText: '',
       loading: false,
       formValues: {},
       inventoryFormDialog: false,
@@ -95,15 +122,88 @@ export default {
         size: 10,
       },
       results: {},
+      headers: [
+        { text: 'Name', value: 'name', sortable: false },
+        { text: 'Description', value: 'description', sortable: false },
+        { text: 'Quantity', value: 'quantity', sortable: false },
+        { text: 'Action', sortable: false },
+      ],
     };
   },
+  watch: {
+    inventoryFormDialog(val) {
+      if (!val) {
+        this.formValues = {};
+      }
+    },
+  },
+  created() {
+    this.fetchData();
+  },
   methods: {
+    getFormAction() {
+      if (this.formValues.id) {
+        return this.updateInventory();
+      }
+      return this.createInventory();
+    },
     fetchData() {
       this.loading = true;
       axios.get('http://localhost:9999/inventory', { params: this.paginationParams })
         .then((resp) => {
+          console.log(resp);
           this.inventoryList = resp.data.content;
           this.results = resp.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      this.loading = false;
+    },
+    createInventory() {
+      this.loading = true;
+      axios.post('http://localhost:9999/inventory', this.formValues)
+        .then(() => {
+          this.formValues = {};
+          this.inventoryFormDialog = false;
+          this.fetchData();
+          this.snackbarText = 'Success! Created Inventory.';
+          this.snackbar = true;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      this.loading = false;
+    },
+    triggerUpdate(inventory) {
+      this.formValues = inventory;
+      this.inventoryFormDialog = true;
+    },
+    updateInventory() {
+      this.loading = true;
+      const updateUrl = `http://localhost:9999/inventory/${this.formValues.id}`;
+      axios.put(updateUrl, this.formValues)
+        .then(() => {
+          this.formValues = {};
+          this.inventoryFormDialog = false;
+          this.fetchData();
+          this.snackbarText = 'Success! Updated inventory.';
+          this.snackbar = true;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      this.loading = false;
+    },
+    deleteInventory(inventory) {
+      this.loading = true;
+      const deleteUrl = `http://localhost:9999/inventory/${inventory.inventoryId}`;
+      axios.delete(deleteUrl)
+        .then(() => {
+          this.formValues = {};
+          this.fetchData();
+          this.snackbarText = 'Success! Deleted inventory.';
+          this.snackbar = true;
         })
         .catch((err) => {
           console.log(err);
